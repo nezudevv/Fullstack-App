aec10dfa43290b63549c43b7758259d7ff19fe72
Matthew Breaux <matt.breaux.dev@gmail.com>
2021-06-27 10:19:11 -0500

    test

--- my-app-backend/ddb_putitem.js
+++ my-app-backend/ddb_putitem.js
@@ -5,8 +5,6 @@ import AWS from "aws-sdk";
 AWS.config.update({
   region: "us-east-2",
   endpoint: "dynamodb.us-east-2.amazonaws.com",
-  accessKeyId: "AKIAWHRUE6ZDXOEFKNWC",
-  secretAccessKey: "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",
 });
 
 // Create the DynamoDB service object
